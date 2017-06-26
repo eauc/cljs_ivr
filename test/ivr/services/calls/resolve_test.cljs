c176f62a18ccf1b15c1f6103edfbe59178e856b5
Auclair Emmanuel <auclair.emmanuel.prestataire@sfr.com>
2017-06-26 12:53:01 +0200

    services/calls/resolve tests

---
+++ test/ivr/services/calls/resolve_test.cljs
@@ -0,0 +1,94 @@
+(ns ivr.services.calls.resolve-test
+	(:require [clojure.test :as test :refer-macros [async deftest is run-tests testing use-fixtures]]
+						[cljs.spec :as spec]
+						[ivr.services.calls.resolve :as resolve]))
+
+(deftest calls-resolve-service
+	(testing "find-or-create-call"
+		(let [db {:calls {"call1" {:id "call1"}}}
+					coeffects {:db db :call-time-now "call-time-now"}
+					params {"account_id" "account-id"
+									"application_id" "application-id"
+									"call_id" "new-call"
+									"from" "from-sda"
+									"to" "to-sda"
+									"script_id" "script-id"}]
+
+
+			(testing "call does not exists, no create?"
+				(is (= {:ivr.routes/response
+								{:status 404
+								 :data {:status 404
+												:status_code "call_not_found"
+												:message "Call not found"
+												:cause {:call-id "new-call"}}}}
+							 (resolve/find-or-create-call
+								 coeffects {:create? false} {:params params}))))
+
+
+			(testing "call does not exists, missing params for creation"
+				(is (= {:ivr.routes/response
+								{:status 400
+								 :data {:status 400
+												:status_code "missing_request_params"
+												:message "Missing request params"
+												:cause {:call-id "new-call"
+																:account-id "missing"
+																:script-id "script-id"}}}}
+							 (resolve/find-or-create-call
+								 coeffects {:create? true}
+								 {:params (merge params {"account_id" nil})})))
+				(is (= {:ivr.routes/response
+								{:status 400
+								 :data {:status 400
+												:status_code "missing_request_params"
+												:message "Missing request params"
+												:cause {:call-id "new-call"
+																:account-id "account-id"
+																:script-id "missing"}}}}
+							 (resolve/find-or-create-call
+								 coeffects {:create? true}
+								 {:params (merge params {"script_id" nil})})))
+				(is (= {:ivr.routes/response
+								{:status 400
+								 :data {:status 400
+												:status_code "missing_request_params"
+												:message "Missing request params"
+												:cause {:call-id "missing"
+																:account-id "account-id"
+																:script-id "script-id"}}}}
+							 (resolve/find-or-create-call
+								 coeffects {:create? true}
+								 {:params (merge params {"call_id" nil})}))))
+
+
+			(testing "call does not exits, create success"
+				(let [expected-call {:info {:id "new-call"
+																		:account-id "account-id"
+																		:application-id "application-id"
+																		:from "from-sda"
+																		:to "to-sda"
+																		:script-id "script-id"
+																		:time "call-time-now"}
+														 :state {:current "Created" :start-time "call-time-now"}
+														 :action-data {}
+														 :action-ongoing nil}]
+					(is (= {:ivr.call/create expected-call
+									:ivr.routes/params (assoc params "call" expected-call)
+                  :ivr.routes/next nil}
+								 (resolve/find-or-create-call
+									 coeffects {:create? true} {:params params})))))
+
+
+			(testing "call exits, proceed"
+				(is (= {:ivr.routes/params {"account_id" "account-id"
+																		"application_id" "application-id"
+																		"from" "from-sda"
+																		"to" "to-sda"
+																		"call_id" "call1"
+																		"script_id" "script-id"
+																		"call" {:id "call1"}}
+								:ivr.routes/next nil}
+							 (resolve/find-or-create-call
+								 coeffects {:create? true}
+								 {:params (merge params {"call_id" "call1"})})))))))
